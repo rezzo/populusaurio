4ff4a845e0812c4871a377a9e737c1fd44fa67fe
J. Pablo Pérez Trabucco <jpereztrabucco@gmail.com>
2015-10-21 08:07:02 -0300

    WIP

--- js/populusaurio.js
+++ js/populusaurio.js
@@ -136,9 +136,21 @@ $( document ).ready(function() {
       dataType: 'json'
     }).success(function(data) {
         var l = data.data.link;
-        uploaded.innerHTML = '<a href="' + l + '" target="_blank">'
-        + l
-        + '</a>';
+        uploaded.innerHTML = '<div class="col-md-2">'
+        + '<div id="fb-root"></div>'
+        + '<script>(function(d, s, id) {'
+        + 'var js, fjs = d.getElementsByTagName(s)[0];'
+        + 'if (d.getElementById(id)) return;'
+        + 'js = d.createElement(s); js.id = id;'
+        + 'js.src = "//connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v2.0";'
+        + 'fjs.parentNode.insertBefore(js, fjs);'
+        + '}(document, "script", "facebook-jssdk"));'
+        + '</script>'
+        + '<div class="fb-like" data-href="' + l + '" data-layout="button_count" data-action="like" data-show-faces="false" data-share="true"></div>'
+        + '</div>';
+        // uploaded.innerHTML = '<a href="' + l + '" target="_blank">'
+        // + l
+        // + '</a>';
     }).error(function(err) {
         console.error(err);
     });
